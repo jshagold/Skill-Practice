de2aac51b71db6b9873ee1b1700b03c6cd603607
hakee <jshagold@gmail.com>
2025-02-21 11:39:55 +0900

    feat: pointerInput 조건부 실행 구현

--- market/src/main/java/com/example/market/present/ui/budget/component/dragdrop/DragDropList.kt
+++ market/src/main/java/com/example/market/present/ui/budget/component/dragdrop/DragDropList.kt
@@ -1,8 +1,10 @@
 package com.example.market.present.ui.budget.component.dragdrop
 
 import android.util.Log
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
@@ -14,6 +16,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import com.example.market.present.utils.extension.dragModifier
 import com.example.market.present.utils.extension.move
 import kotlinx.coroutines.Job
@@ -25,9 +28,9 @@ fun DragDropList(
     modifier: Modifier,
     listSize: Int,
     changeListIndex: (from: Int, to: Int) -> Unit,
+    isActivate: Boolean = true,
     content: @Composable (modifier: Modifier, index: Int) -> Unit
 ) {
-    val list = remember { mutableStateListOf(1,2,3,4,5,6,7,8) }
     val lazyListState = rememberLazyListState()
     val dragAndDropListState =
         rememberDragDropListState(lazyListState) { from, to ->
@@ -42,35 +45,39 @@ fun DragDropList(
 
     LazyColumn(
         modifier = modifier
-            .pointerInput(Unit) {
-                detectDragGesturesAfterLongPress(
-                    onDrag = { change, offset ->
-                        change.consume()
-                        dragAndDropListState.onDrag(offset)
+            .pointerInput(isActivate) {
+                Log.e("TAG", "DragDropList: isActivate: $isActivate", )
+                if(isActivate) {
+                    detectDragGesturesAfterLongPress(
+                        onDrag = { change, offset ->
+                            change.consume()
+                            dragAndDropListState.onDrag(offset)
 
-                        if(overScrollJob?.isActive == true) return@detectDragGesturesAfterLongPress
+                            if(overScrollJob?.isActive == true) return@detectDragGesturesAfterLongPress
 
-                        dragAndDropListState
-                            .checkOverscroll()
-                            .takeIf { it != 0f }
-                            ?.let {
-                                overScrollJob = coroutineScope.launch {
-                                    dragAndDropListState.lazyListState.scrollBy(it)
-                                }
-                            } ?: run { overScrollJob?.cancel() }
-                    },
-                    onDragStart = { offset ->
-                        dragAndDropListState.onDragStart(offset)
-                    },
-                    onDragEnd = {
-                        dragAndDropListState.onDragInterrupted()
-                    },
-                    onDragCancel = {
-                        dragAndDropListState.onDragInterrupted()
-                    },
-                )
+                            dragAndDropListState
+                                .checkOverscroll()
+                                .takeIf { it != 0f }
+                                ?.let {
+                                    overScrollJob = coroutineScope.launch {
+                                        dragAndDropListState.lazyListState.scrollBy(it)
+                                    }
+                                } ?: run { overScrollJob?.cancel() }
+                        },
+                        onDragStart = { offset ->
+                            dragAndDropListState.onDragStart(offset)
+                        },
+                        onDragEnd = {
+                            dragAndDropListState.onDragInterrupted()
+                        },
+                        onDragCancel = {
+                            dragAndDropListState.onDragInterrupted()
+                        },
+                    )
+                }
             },
         state = dragAndDropListState.lazyListState,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         items(listSize) { index ->
             content(
